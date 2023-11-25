b7dc8a79e31751a375ea57e256b57370b5bfe56b
Cristian <cristian-02-@live.com>
2023-11-24 22:25:19 -0500

    Conexion con postgresql'

--- applications/empleados/models.py
+++ applications/empleados/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+# Imports the model
+from applications.departamento.models import Departamento
+
 # Create your models here.
+class Empleado(models.Model):
+    """ Modelo para tabla empleado """
+
+    JOB_CHOICES = (
+        ('0', 'CONTADOR'),
+        ('1', 'ADMINISTRADOR'),
+        ('2', 'ECONOMISTA'),
+        ('3', 'OTRO'),
+    )
+
+    first_name = models.CharField('Nombres', max_length=60)
+    last_name = models.CharField('Apellidos', max_length=60)
+    job = models.CharField('Trabajos', choices=JOB_CHOICES, max_length=1)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    # image = models.ImageField('Imagen', upload_to=None, height_field=None, width_field=None, max_length=None)
+
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.first_name + ' - ' + self.last_name
--- empleado/settings/local.py
+++ empleado/settings/local.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbempleado',
+        'USER': 'monchi789',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
