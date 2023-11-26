8a92e23d6cff231c429537387b4018f285e8d592
Cristian <cristian-02-@live.com>
2023-11-25 23:37:35 -0500

    Programando un sabado por la noche 🤑

--- applications/empleados/models.py
+++ applications/empleados/models.py
@@ -20,6 +20,12 @@ class Empleado(models.Model):
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     # image = models.ImageField('Imagen', upload_to=None, height_field=None, width_field=None, max_length=None)
 
+    class Meta:
+        verbose_name = 'Mi empleado'
+        verbose_name_plural = 'Mis empleados'
+        ordering = ['first_name']
+        unique_together = ('first_name', 'last_name')
+
 
     def __str__(self):
         return str(self.id) + ' - ' + self.first_name + ' - ' + self.last_name
